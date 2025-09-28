a2348a0c3e8aeb3c859ab3d9a83346d6b545ce3c
DevEcho625 <anuj.m.bhatia@gmail.com>
2025-09-28 22:30:10 +0530

    Update script.js
    adding some more points

--- wildfire-map/script.js
+++ wildfire-map/script.js
@@ -10,6 +10,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var fireData = [
   { lat: 38.5, lon: -121.5 }, // Sacramento area
   { lat: 34.2, lon: -118.4 }, // Los Angeles area
+  { lat: 36.7, lon: -119.8 },  // Fresno area
+    { lat: 39.0, lon: -120.5 } , // Chico area
+    { lat: 34.5, lon: -117.3 },  // San Bernardino area
+    { lat: 32.7, lon: -117.1 } , // San Diego area
+    { lat: 38.0, lon: -122.3 },  // Napa area
 ];
 
 fireData.forEach(fire => {
